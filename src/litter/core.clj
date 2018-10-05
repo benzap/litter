53c7268895d24676e470ed3c450e4f562d5eccd6
Benjamin Zaporzan <ben@district0x.io>
2018-10-04 20:24:56 -0400

    First Commit

---
+++ src/litter/core.clj
@@ -0,0 +1,6 @@
+(ns litter.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/litter/core_test.clj
@@ -0,0 +1,7 @@
+(ns litter.core-test
+  (:require [clojure.test :refer :all]
+            [litter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
